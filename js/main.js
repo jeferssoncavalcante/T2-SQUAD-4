e6be65f41906e03619262032ca144dbada9188dc
Jeferson Cavalcante <jeferson.mdc@hotmail.com>
2021-04-21 17:45:02 -0300

    update project

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
+// Carregando API e Convertendo em dados JSON //
+const showData = (result)=>{
+    for (const campo in result) {
+        console.log(campo)
+    }
+}
 let options = {
     method: 'GET',
     mode: 'cors',
     cache: 'default'
 }
+valor = new Array()
+function carregaJSON(filename) {
 fetch(`https://economia.awesomeapi.com.br/json/daily/BTC-BRL/15`, options)
-    .then(Response => {
-        Response.json()
-            .then(data => console.log(data))
-    })
-    .catch(e => console.log(`Deu Erro !` + e, message))
-monName = new Array("janeiro", "fevereiro", "março", "abril", "maio", "junho", "agosto", "outubro", "novembro", "dezembro")
+.then(response => response.json())
+    .then(data => {console.log(data);
+    dados = data
+    for (var i = 0; i < dados.length; i++){
+        valor.push(dados[i]["bid"])
+    };
+})    
+}
+carregaJSON();
+// -------------------------------------------- //
+// Criando Array com os nomes dos meses e atribuindo o dia //
+monName = new Array("janeiro", "fevereiro", "março", "abril", "maio", "junho", "agosto", "outubro", "novembro", "dezembro");
 hoje = new Date;
 /*const labels = Array.from({ length: 7 }, (item, i) => {
     return new Date(0, i).toLocaleString('pt-BR', { month: 'long' })
@@ -17,7 +31,9 @@ hoje = new Date;
 var atual = 600000.00;
 var f = atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
 console.log(f)*/
-const data = {
+// ------------------------------------------- //
+// Atribuindo os Dados ao Gráfico //
+const data1 = {
     labels: [`${hoje.getDate()} ${monName[hoje.getMonth()]}`,
     `${hoje.getDate() - 1} ${monName[hoje.getMonth()]}`,
     `${hoje.getDate() - 2} ${monName[hoje.getMonth()]}`,
@@ -36,7 +52,7 @@ const data = {
     ],
     datasets: [{
         label: 'Testando Gráfico',
-        data: [65, 59, 80, 81, 56, 55, 49, 90, 100, 87, 56, 25],
+        data: [valor[0], valor[1], valor[2]],
         backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
             'rgba(255, 159, 64, 0.2)',
@@ -58,9 +74,11 @@ const data = {
         borderWidth: 1
     }]
 };
-const config = {
+// ---------------------------------------- //
+// Definindo o tipo de grafico e suas configurações //
+const config1 = {
     type: 'line',
-    data: data,
+    data: data1,
     options: {
         scales: {
             y: {
@@ -69,6 +87,123 @@ const config = {
         }
     },
 };
+// --------------------------------------------- //
+// Iniciando Gráfico de Linhas //
 var myChart = new Chart(document.getElementById('Grafico1'),
-    config
+    config1
 );
+// ---------------------------------------------- //
+// Criando Grafico 2 //
+const data2 = {
+    labels: [`${hoje.getDate()} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 1} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 2} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 3} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 4} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 5} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 6} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 7} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 8} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 9} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 10} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 11} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 12} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 13} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 14} ${monName[hoje.getMonth()]}`,
+    ],
+    datasets: [{
+        label: 'Testando Gráfico',
+        data: [],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(201, 203, 207, 0.2)'
+        ],
+        borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+            'rgb(201, 203, 207)'
+        ],
+        borderWidth: 1
+    }]
+};
+const config2 = {
+    type: 'bar',
+    data: data2,
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+            }
+        }
+    },
+};
+var myChart = new Chart(document.getElementById('Grafico2'),
+    config2
+);
+// ---------------------------------- //
+// Criando Grafico 3 //
+const data3 = {
+    labels: [`${hoje.getDate()} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 1} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 2} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 3} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 4} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 5} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 6} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 7} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 8} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 9} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 10} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 11} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 12} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 13} ${monName[hoje.getMonth()]}`,
+    `${hoje.getDate() - 14} ${monName[hoje.getMonth()]}`,
+    ],
+    datasets: [{
+        label: 'Testando Gráfico',
+        data: [],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(201, 203, 207, 0.2)'
+        ],
+        borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+            'rgb(201, 203, 207)'
+        ],
+        borderWidth: 1
+    }]
+};
+const config3 = {
+    type: 'radar',
+    data: data3,
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+            }
+        }
+    },
+};
+var myChart = new Chart(document.getElementById('Grafico3'),
+    config3
+);
+// ---------------------------------------------- //
